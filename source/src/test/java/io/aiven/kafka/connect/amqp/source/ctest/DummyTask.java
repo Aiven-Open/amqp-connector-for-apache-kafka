7338904e6718858cc18e1ef934d88248de0386d8
Claude Warren <claude.warren@aiven.io>
2026-04-16 09:02:47 +0100

    added tests

---
+++ source/src/test/java/io/aiven/kafka/connect/amqp/source/ctest/DummyTask.java
@@ -0,0 +1,43 @@
+/*
+        Copyright 2026 Aiven Oy and project contributors
+
+       Licensed under the Apache License, Version 2.0 (the "License");
+       you may not use this file except in compliance with the License.
+       You may obtain a copy of the License at
+
+       https://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+
+       SPDX-License-Identifier: Apache-2.0
+*/
+package io.aiven.kafka.connect.amqp.source.ctest;
+
+import java.util.Map;
+import org.apache.kafka.connect.connector.Task;
+
+public class DummyTask implements Task {
+
+  static int startCount = 0;
+  static int stopCount = 0;
+
+  @Override
+  public String version() {
+    return "Dummy Task";
+  }
+
+  @Override
+  public void start(Map<String, String> props) {
+    ++startCount;
+  }
+
+  @Override
+  public void stop() {
+    ++stopCount;
+  }
+}
